f861884e92981cffd18a04bd43bc4e8e2c263234
Ariane Viana <ariane.vianads@gmail.com>
2022-06-14 17:26:37 -0300

    props, css, fragments

--- aulas/src/App.js
+++ aulas/src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa'
+import Frase from './components/Frase';
+import List from './components/List';
+
 
 function App() {
-
-  const name = "Ariane"
-  const newName = name.toUpperCase(name)
-
-  function sum(a,b){
-    return a + b
-  }
-
-  const url = "https://www.skoob.com.br/"
-
-  const urlImagem = "https://via.placeholder.com/150"
+  const nome = "Simba"
 
   return (
-    <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá {newName}</p>
-      <p>Soma: {sum(2,3)}</p>
-      <p><a href={url} target="_blank">Skoob</a></p>
-      <img src={urlImagem} alt="Minha imagem"/>
-      <HelloWorld />
+    <div className="App">   
+      <h1>Testando CSS</h1> 
+      <Frase />
+
+      <h1>Testando Props</h1>  
+      <SayMyName nome="Ariane" />
+      <SayMyName nome="Gustavo" />
+      <SayMyName nome={nome}/>
+      <Pessoa
+        nome="Ariane"
+        idade = {31}
+        profissao = "Estudante" 
+        foto = "https://via.placeholder.com/150" />   
+
+      <List />
     </div>
   );
 }
