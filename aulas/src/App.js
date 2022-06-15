b14faace0919c00f487a930f36e0278330600e4d
ArianeViana <ariane.vianads@gmail.com>
2022-06-15 10:33:21 -0300

    renderização de listas

--- aulas/src/App.js
+++ aulas/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import Condicional from './components/Condicional';
-
+import ListaRend from './components/ListaRend';
 
 function App() {
 
+  const meusItens = ["react", "vue", "angular"]
+
   return (
     <div className="App">   
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+      <h1>Renderização de Listas</h1>
+      <ListaRend itens={meusItens} />
+      <h2>Lista vazia</h2>
+      <ListaRend itens={[]}/>
+      
     </div>
-  );
+  )
 }
 
 export default App;
